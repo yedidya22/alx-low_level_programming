cc8793aa43377984c389aa7a5e1457735ef066b7
yedidya22 <yedidyasole@gmail.com>
2021-09-09 10:18:51 -0700

    array and pointer

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - Compares pointers to two strings.
+ * @s1: A pointer to the first string to be compared.
+ * @s2: A pointer to the second string to be compared.
+ *
+ * Return: outputs the difference of s1 and s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+while (*s1 && *s2 && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+
+return (*s1 - *s2);
+}
